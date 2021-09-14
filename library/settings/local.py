e1b6054137c0082b9c246731e70f9576a094de2a
Jaolmos <jose.a.olmosmartinez@gmail.com>
2021-09-14 19:58:07 +0200

    postgresql database connects

--- library/settings/local.py
+++ library/settings/local.py
@@ -6,8 +6,11 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dblibrary',
+        'USER': 'joseadb',
+        'PASSWORD': 'djangopass',
+        'PORT': ('5432'),
     }
 }
 
