1918c78db6c7b6f3030e0f207069b48c09d3d850
Jaolmos <jose.a.olmosmartinez@gmail.com>
2021-09-15 07:44:49 +0200

    author model is created

--- applications/author/models.py
+++ applications/author/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
+    nationality = models.CharField(max_length=40)
+    age = models.PositiveIntegerField()
+
+def __str__(self):
+    return self.first_name + ' ' + self.last_name
