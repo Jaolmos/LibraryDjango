46b364a5f2ff8c7ca76d1968a3fa301a21c0d317
Jaolmos <jose.a.olmosmartinez@gmail.com>
2021-09-17 20:53:26 +0200

    update

--- applications/author/views.py
+++ applications/author/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
-
 from django.views.generic import ListView
-
 from . models import Author
-
 from . managers import AuthorManager
 
 class ListAuthors(ListView):
