d98167198d90e72a1c8d00e3787d397aada6755a
Jaolmos <jose.a.olmosmartinez@gmail.com>
2021-09-20 19:14:51 +0200

    add query foreignkey

--- applications/book/managers.py
+++ applications/book/managers.py
@@ -5,9 +5,13 @@ class BookManager(models.Manager):
 
     def search_book(self, keyword):
         outcome = self.filter(title__icontains=keyword)
-        
         return outcome
 
     def list_books_category(self, category):
-
         return self.filter(category__id=category).order_by('title')
+
+
+class CategoryManager(models.Manager):
+
+    def category_by_author(self, author):
+        return self.filter(book_category__authors__id=author)        
--- applications/book/models.py
+++ applications/book/models.py
@@ -2,24 +2,30 @@ from django.db import models
 
 from applications.author.models import Author
 
-from .managers import BookManager
+from .managers import BookManager, CategoryManager
 
 
 # Create your models here.
 
 class Category(models.Model):
     name = models.CharField(max_length=30)
+    objects = CategoryManager()
     
     def __str__(self):
         return str(self.id) + '-' + self.name
 
 class Book(models.Model):
     title = models.CharField(max_length=60, verbose_name='Title')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Category')
+    category = models.ForeignKey(
+    Category, on_delete=models.CASCADE,
+    verbose_name='Category',
+    related_name = 'book_category'
+    )
     authors = models.ManyToManyField(Author, verbose_name='Autor')
     date = models.DateField(verbose_name='Release date')  
     book_cover = models.ImageField(verbose_name='Book cover', upload_to='Image')     
     visits = models.PositiveIntegerField(verbose_name='Visits')
+
 
     objects = BookManager()
 
